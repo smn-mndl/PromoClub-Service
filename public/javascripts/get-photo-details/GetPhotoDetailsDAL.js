205bb3f653cceb378fef3afe2cb6d1b2ee395baf
smn-mndl <45252675+smn-mndl@users.noreply.github.com>
2022-01-09 11:09:42 +0530

    changes to database

--- public/javascripts/get-photo-details/GetPhotoDetailsDAL.js
+++ public/javascripts/get-photo-details/GetPhotoDetailsDAL.js
@@ -1,8 +1,9 @@
 const MongoClient = require("mongodb");
 const connURL =
+  process.env.MONGODB_URI3 || "mongodb://127.0.0.1:27017/PromoClub";
+const connURL2 =
   process.env.MONGODB_URI2 || "mongodb://127.0.0.1:27017/PromoClub";
-
-const collectionName = "LatestPhotos";
+const collectionName = "AllPhotosCollection";
 const isEmpty = require("lodash/isEmpty");
 
 const photoDetails = {};
@@ -18,14 +19,34 @@ photoDetails.photoDetailsDAL = (payload) => {
       .find({ _id: payload.id })
       .toArray()
       .then((data) => {
+        console.log(data, "data ");
         if (data.length === 1) {
-          return {
-            result: data,
-            status: "success",
-          };
+          return MongoClient.connect(connURL2, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            keepAlive: 1,
+          }).then((client2) => {
+            const connct1 = client2.db().collection(data[0]["albumName"]);
+            return connct1
+              .find({ _id: payload.id })
+              .toArray()
+              .then((data) => {
+                if (data.length === 1) {
+                  return {
+                    result: data,
+                    status: "success",
+                  };
+                } else {
+                  return {
+                    result: [],
+                    status: "success",
+                  };
+                }
+              });
+          });
         } else {
           return {
-            result: data,
+            result: [],
             status: "success",
           };
         }
